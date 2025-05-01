ddef25301b19e32e1a0e251141e57ff4aafeaa4b
DeodatoBastos <deodatobasto@gmail.com>
2025-05-01 09:24:53 +0200

    update augmentation script

--- localization/scripts/run_augmentation.py
+++ localization/scripts/run_augmentation.py
@@ -6,6 +6,10 @@ from scipy.interpolate import Rbf
 
 from localization import dataset
 from localization.augmentation.augmentation_rbf_model import Augmentation
+from localization.models.rbf_model import Model
+
+def radial_log_basis_function(self, r):
+    return np.log(r + self.epsilon)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run tLoc')
@@ -13,19 +17,17 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--num_samples', type=int, default=1620, help='Number of samples per floor')
     parser.add_argument('-b','--batch_size', type=int, default=180, help='Batch Size')
     parser.add_argument('-t', '--test_size', type=float, default=0.06, help='Size of the test dataset')
+    parser.add_argument('-f', '--data_folder', type=str, default="data/generated", help='Folder to save the generated data')
     args = parser.parse_args()
 
     data = dataset.load_ujiindoor_loc("data/filtered")
 
-    def radial_log_basis_function(self, r):
-        return np.log(r + self.epsilon)
-
     Rbf.radial_log_basis_function = radial_log_basis_function
     with open('output/filtered_model.bin', 'rb') as inp:
-        model = pickle.load(inp)
+        model: Model = pickle.load(inp)
 
     routers = model.get_all_routers_in_this_floor(data.get_full_df().columns)
     augmenter = Augmentation(model, routers)
     augmenter.generate_augmented_data_batched(num_samples_per_floor=args.num_samples,
                                               batch_size=args.batch_size, test_size=args.test_size,
-                                              data_dir="data/generated")
+                                              data_dir=args.data_folder)
