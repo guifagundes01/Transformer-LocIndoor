ab7db197f8dc0aab674f4eafa67d32a2882d48e8
DeodatoBastos <deodatobasto@gmail.com>
2025-05-03 21:27:56 +0200

    fix style problems

--- localization/models/_flatten.py
+++ localization/models/_flatten.py
@@ -4,13 +4,10 @@ from torch import nn
 from torch import Tensor
 
 class Flatten(nn.Module):
-
     def __init__(self, num_neurons_flatten: int):
-      
         """
-        
         Constructor for the Flatten module.
-        
+
         Module that flattens the input tensor into a 2D tensor.
 
         Args:
@@ -18,15 +15,12 @@ class Flatten(nn.Module):
 
         Attributes:
         - num_neurons_flatten (int): number of neurons to flatten the input tensor to
-        
         """
         super(Flatten, self).__init__()
         self.num_neurons_flatten = num_neurons_flatten
 
     def forward(self, x: Tensor) -> Tensor:
-      
         """
-        
         Forward pass of the Flatten module.
 
         Args:
@@ -34,7 +28,6 @@ class Flatten(nn.Module):
 
         Returns:
         - out (Tensor): flattened tensor
-        
         """
         out = x.contiguous().view(-1, self.num_neurons_flatten)
         return out
--- localization/models/_positionalencoding.py
+++ localization/models/_positionalencoding.py
@@ -6,11 +6,8 @@ from torch import nn
 from torch import Tensor
 
 class PositionalEncoding(nn.Module):
-
     def __init__(self, d_model: int, max_len: int, dropout: float = 0.1):
-    
         """
-
         Constructor for the PositionalEncoding class.
         Based on: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 
@@ -25,48 +22,43 @@ class PositionalEncoding(nn.Module):
                          generated using sine and cosine functions based on position and 
                          exponential decay of frequency.
         """
-            
-        
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
         # Generate position indices
         position = torch.arange(max_len).unsqueeze(1)
-        
+
         # Calculate the div_term
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        
+
         # Generate the positional encoding values
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
-        
+
         # Register the positional encoding as a buffer
         self.register_buffer('pe', pe)
 
     def forward(self, x: Tensor) -> Tensor:
-    
         """
-        
         Applies positional encoding to a sequence of embeddings.
-        
+
         Args:
             x: Tensor - A tensor of shape [batch_size, seq_len, embedding_dim].
-            
+
         Returns:
             Tensor - A tensor of shape [batch_size, seq_len, embedding_dim] with positional encoding applied.
-            
+
         """
-        
         # Transpose to [seq_len, batch_size, embedding_dim]
         x = x.transpose(0,1)
-        
+
         # Add positional encoding to the embeddings
         x = x + self.pe[:x.size(0)]
-        
+
         # Apply dropout
         x = self.dropout(x)
-        
+
         # Transpose back to [batch_size, seq_len, embedding_dim]
         x = x.transpose(1,0)
         return x
