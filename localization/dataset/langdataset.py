54a616c7b7aa04bd62d08cef3ca58acbfc0ffd54
DeodatoBastos <deodatobasto@gmail.com>
2025-05-10 08:32:55 +0200

    fix processing data for lang problem

--- localization/dataset/langdataset.py
+++ localization/dataset/langdataset.py
@@ -1,28 +1,29 @@
-from typing import List, Tuple, Any
+from typing import Tuple
 
 import torch
-from numpy.typing import NDArray
+import numpy as np
 from torch.nn.functional import pad
 from torch.utils.data.dataset import Dataset
 
 class LangDataset(Dataset):
     def __init__(self, file_path: str, device, src_dim: int, sos_idx: int, padding_idx = 0):
-        self.data: List[Tuple[NDArray[Any], NDArray[Any]]] = torch.load(file_path, weights_only=False)
-        self.device = device
-        self.padding_idx = padding_idx
-        self.sos_idx = sos_idx
-        self.src_dim = src_dim
+        data = torch.load(file_path, weights_only=False)
+        seqs, tars = zip(*data)
+        self.sequences = torch.empty((len(seqs), src_dim), dtype=torch.long)
+        for i, seq in enumerate(seqs):
+            seq = torch.tensor(seq, dtype=torch.long)
+            seq = pad(seq + 1, (1, 0), value=sos_idx)
+            if seq.size(0) < src_dim:
+                seq = pad(seq, (0, src_dim - seq.size(0)), value=padding_idx)
+            else:
+                seq = seq[:src_dim]
+            self.sequences[i] = seq
 
-    def __getitem__(self, index: int):
-        seq, target = self.data[index]
-        seq = torch.tensor(seq, dtype=torch.int32).to(self.device)
-        target = torch.tensor(target, dtype=torch.float32).to(self.device)
-        seq = torch.cat((torch.tensor(self.sos_idx), seq+1))
-        if seq.shape[0] < self.src_dim:
-            seq = pad(seq, (0, self.src_dim - seq.shape[0]), "constant", self.padding_idx)
-        else:
-            seq = seq[:self.src_dim]
-        return seq, target
+        self.sequences = self.sequences.to(device)
+        self.targets = torch.tensor(np.array(tars), dtype=torch.float32).to(device)
+
+    def __getitem__(self, index: int)  -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.sequences[index], self.targets[index]
 
     def __len__(self):
-        return len(self.data)
+        return len(self.targets)
